a25679c59a47af62f1d283c69fc93de9e268b14d
Daniel Tsai <59071439+daniel8691@users.noreply.github.com>
2020-12-23 15:19:35 -0500

    added burger css file and added dynamic component for the burger depending on user input

---
+++ react-app/src/Components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import './BurgerIngredient.css';
+
+
+const burgerIngredient = (props) => {
+    let ingredient = null;
+// expect to receive the type
+// ingredient is either null, or one of the many "DIVs"
+    switch (props.type) {
+        case ('bread-bottom'):
+            ingredient = <div className="BreadBottom"></div>
+            break;
+        case ('bread-top'):
+            ingredient = (
+                <div className="BreadTop">
+                    <div className="Seeds1"></div>
+                    <div className="Seeds2"></div>
+                </div>
+            );
+            break;
+        case('meat'):
+            ingredient = <div className="Meat"></div>
+            break;
+        case("cheese"):
+            ingredient = <div className="Cheese"></div>
+            breal;
+        case("salad"):
+            ingredient = <div className="Salad"></div>
+            break;
+        case("bacon"):
+            ingredient = <div className="Bacon"></div>
+            break;
+        default:
+            ingredient=null;
+ 
+
+    }
+
+};
+
+export default burgerIngredient;
